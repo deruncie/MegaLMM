8d3e2e5cb255b46f9ea5f91c39874873885a2357
deruncie <deruncie@ucdavis.edu>
2022-05-31 14:35:36 -0700

    Update Utilities.cpp

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -30,7 +30,7 @@ void record_sample_Posterior_array(Map<MatrixXd> current_sample, Map<MatrixXd> P
 void set_MegaLMM_nthreads(int threads) {
   if ( threads > 0 ) {
     // REprintf("not currently implemented");
-      omp_set_num_threads( threads );
+      // omp_set_num_threads( threads );
   }
   // REprintf("Number of threads=%i\n", omp_get_max_threads());
 }
